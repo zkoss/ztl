6cf85a7c1d48d7daff15ba687ef0761140401b4a
Jumper Chen <jumperchen@potix.com>
2014-05-26 03:30:53 +0100

    fine tune for Opera

--- ztl/src/main/java/org/zkoss/ztl/ZKClientTestCase.java
+++ ztl/src/main/java/org/zkoss/ztl/ZKClientTestCase.java
@@ -827,6 +827,7 @@ public class ZKClientTestCase extends ZKTestCase {
 		if (isOpera()) {
 			findElement(locator).clear();
 			waitResponse();
+			focus(locator);
 			typeKeys(locator, value);
 			waitResponse();
 		} else {
