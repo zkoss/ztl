fafe7cf7c8a38bf29281e47494ff7642d306c185
jerrychen <jerrychen@potix.com>
2014-05-29 05:46:41 +0100

    add log

--- ztl/src/main/java/org/zkoss/ztl/ConnectionManager.java
+++ ztl/src/main/java/org/zkoss/ztl/ConnectionManager.java
@@ -118,7 +118,8 @@ public class ConnectionManager {
 					return remote;
 				}
 			} catch (Exception e) {
-				System.out.println(random + ":can't create or open a file to write");
+				System.out.println(random + ":can't create or open a file to write: " + e.getMessage());
+				e.printStackTrace();
 			} finally {
 				if(lock == null)
 					try {
