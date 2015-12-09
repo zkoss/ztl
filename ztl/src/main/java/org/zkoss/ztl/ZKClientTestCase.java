78013f436835ffbe93bd8162f3c57e97f783af50
christopher-szu <christopherszu@potix.com>
2015-12-09 16:01:13 +0800

    fix drag and drop for ie9

--- ztl/src/main/java/org/zkoss/ztl/ZKClientTestCase.java
+++ ztl/src/main/java/org/zkoss/ztl/ZKClientTestCase.java
@@ -349,8 +349,8 @@ public class ZKClientTestCase extends ZKTestCase {
 			int x1 = (int) Double.parseDouble(tos[0]);
 			int y1 = (int) Double.parseDouble(tos[1]);
 			WebElement element = findElement(locatorOfObjectToBeDragged);
-			getActions().moveToElement(element, x0, y0).clickAndHold(element)
-				.moveByOffset(x1-x0, y1-y0).release(null).perform();
+			getActions().moveToElement(element, x0, y0).clickAndHold()
+				.moveByOffset(x1-x0, y1-y0).release().perform();
 		} else {
 			// fixed for Selenium 2.5.0 issue
 			Scripts.triggerMouseEventAt(getWebDriver(), locatorOfObjectToBeDragged, "mousemove", to);
