e157be115836786696c8b57c4d5b423721508798
peggypeng <peggypeng@potix.com>
2026-02-24 11:23:51 +0800

    restore whitespace normalization for innerText

--- ztl/src/main/java/org/zkoss/ztl/ZKClientTestCaseCafe.java
+++ ztl/src/main/java/org/zkoss/ztl/ZKClientTestCaseCafe.java
@@ -1664,7 +1664,7 @@ public class ZKClientTestCaseCafe extends ZKClientTestCase {
 		if (!_isTestCafe) {
 			return super.getText(locator);
 		}
-		return toCafeSelector(locator.toLocator()) + ".innerText";
+		return toCafeSelector(locator.toLocator()) + ".innerText.replace(/\\s/g, ' ')";
 	}
 
 	@Override
